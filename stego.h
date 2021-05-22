9772bad38e56157d85462f3d2781fb3b05fc16c9
BenDemandt <79916437+BenDemandt@users.noreply.github.com>
2021-05-22 15:48:19 +0200

    Add files via upload

---
+++ stego.h
@@ -0,0 +1,10 @@
+#ifndef STEGO_H_FILE
+#include <stdio.h>
+#define STEGO_H_FILE
+
+int read_ppm_type(FILE *);
+int get_width(FILE *);
+int get_height(FILE *);
+int read_color_depth(FILE *);
+
+#endif
